e43b8665c43e59fdbfc88f4430ec3636cbc762a5
Jesus Vazquez <75946803+JesusVazquez-Code@users.noreply.github.com>
2022-02-14 17:27:46 -0600

    Updated for Spring 2022 HW3

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
 __author__ = 'cs540-testers'
-__credits__ = ['Harrison Clark', 'Stephen Jasina', 'Saurabh Kulkarni','Alex Moon']
-version = 'v0.2.2'
+__credits__ = ['Harrison Clark', 'Stephen Jasina', 'Saurabh Kulkarni','Alex Moon','Jesus Vazquez']
+version = 'v0.2.2-modifiedSpring2022'
 
 import sys
 import unittest
 import numpy as np
-from pca import load_and_center_dataset, get_covariance, get_eig, \
-		get_eig_perc, project_image, display_image
+from hw3 import load_and_center_dataset, get_covariance, get_eig, \
+		get_eig_prop, project_image, display_image
 
-mnist_path = 'mnist.npy'
+mnist_path = 'YaleB_32x32.npy'
 
 class TestLoadAndCenterDataset(unittest.TestCase):
 	def test_load(self):
 		x = load_and_center_dataset(mnist_path)
 
 		# The dataset needs to have the correct shape
-		self.assertEqual(np.shape(x), (2000, 784))
+		self.assertEqual(np.shape(x), (2414, 1024))
 
 		# The dataset should not be constant-valued
 		self.assertNotAlmostEqual(np.max(x) - np.min(x), 0)
@@ -33,7 +33,7 @@ class TestGetCovariance(unittest.TestCase):
 		S = get_covariance(x)
 
 		# S should be square and have side length d
-		self.assertEqual(np.shape(S), (784, 784))
+		self.assertEqual(np.shape(S), (1024, 1024))
 
 	def test_values(self):
 		x = load_and_center_dataset(mnist_path)
@@ -53,18 +53,18 @@ class TestGetEig(unittest.TestCase):
 
 		self.assertEqual(np.shape(Lambda), (2, 2))
 		self.assertTrue(np.all(np.isclose(
-				Lambda, [[350880.76329673, 0], [0, 245632.27295307]])))
+				Lambda, [[1369142.41612494, 0], [0, 1341168.50476773]])))
 
 		# The eigenvectors should be the columns
-		self.assertEqual(np.shape(U), (784, 2))
+		self.assertEqual(np.shape(U), (1024, 2))
 		self.assertTrue(np.all(np.isclose(S @ U, U @ Lambda)))
 
 	def test_large(self):
 		x = load_and_center_dataset(mnist_path)
 		S = get_covariance(x)
-		Lambda, U = get_eig(S, 784)
+		Lambda, U = get_eig(S, 1024)
 
-		self.assertEqual(np.shape(Lambda), (784, 784))
+		self.assertEqual(np.shape(Lambda), (1024, 1024))
 		# Check that Lambda is diagonal
 		self.assertEqual(np.count_nonzero(
 				Lambda - np.diag(np.diagonal(Lambda))), 0)
@@ -73,30 +73,30 @@ class TestGetEig(unittest.TestCase):
 				sorted(np.diagonal(Lambda), reverse=True))))
 
 		# The eigenvectors should be the columns
-		self.assertEqual(np.shape(U), (784, 784))
+		self.assertEqual(np.shape(U), (1024, 1024))
 		self.assertTrue(np.all(np.isclose(S @ U, U @ Lambda)))
 
 class TestGetEigPerc(unittest.TestCase):
 	def test_small(self):
 		x = load_and_center_dataset(mnist_path)
 		S = get_covariance(x)
-		Lambda, U = get_eig_perc(S, .07)
+		Lambda, U = get_eig_prop(S,0.07)
 
 		self.assertEqual(np.shape(Lambda), (2, 2))
 		self.assertTrue(np.all(np.isclose(
-				Lambda, [[350880.76329673, 0], [0, 245632.27295307]])))
+				Lambda, [[1369142.41612494, 0], [0, 1341168.50476773]])))
 
 		# The eigenvectors should be the columns
-		self.assertEqual(np.shape(U), (784, 2))
+		self.assertEqual(np.shape(U), (1024, 2))
 		self.assertTrue(np.all(np.isclose(S @ U, U @ Lambda)))
 
 	def test_large(self):
 		x = load_and_center_dataset(mnist_path)
 		S = get_covariance(x)
 		# This will select all eigenvalues/eigenvectors
-		Lambda, U = get_eig_perc(S, -1)
+		Lambda, U = get_eig_prop(S, -1)
 
-		self.assertEqual(np.shape(Lambda), (784, 784))
+		self.assertEqual(np.shape(Lambda), (1024, 1024))
 		# Check that Lambda is diagonal
 		self.assertEqual(np.count_nonzero(
 				Lambda - np.diag(np.diagonal(Lambda))), 0)
@@ -105,7 +105,7 @@ class TestGetEigPerc(unittest.TestCase):
 				sorted(np.diagonal(Lambda), reverse=True))))
 
 		# The eigenvectors should be the columns
-		self.assertEqual(np.shape(U), (784, 784))
+		self.assertEqual(np.shape(U), (1024, 1024))
 		self.assertTrue(np.all(np.isclose(S @ U, U @ Lambda)))
 
 class TestProjectImage(unittest.TestCase):
@@ -116,9 +116,9 @@ class TestProjectImage(unittest.TestCase):
 		# This is the image of the "9" in the spec
 		projected = project_image(x[3], U)
 
-		self.assertEqual(np.shape(projected), (784,))
-		self.assertAlmostEqual(np.min(projected), -113.79455198736488)
-		self.assertAlmostEqual(np.max(projected), 120.0658469887994)
+		self.assertEqual(np.shape(projected), (1024,))
+		self.assertAlmostEqual(np.min(projected), -102.98135151709695)
+		self.assertAlmostEqual(np.max(projected), -2.9426401819431263)
 
 if __name__ == '__main__':
 	# Hack to allow different locations of mnist.npy (done this way to allow
